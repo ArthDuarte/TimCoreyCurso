913393b356d02536a34b803c751fad3a38f1dc1c
Arthur Cardoso <arthur.duartec@gmail.com>
2025-01-29 21:28:50 -0300

    Create int.cs

---
+++ int.cs
@@ -0,0 +1,17 @@
+// ESTUDO SOBRE  NUMEROS INTEIROS
+//numeros inteiros como 2, 18, 295, -37, etc
+// e não como  1.3 , 8.9 etc
+
+int age = 0;
+
+age = 30;
+// Inteiros sempre irão ignorar decimais, então cuidado ao devidir, multiplicar etc (Regra: não divida ints)
+
+//não usamos "" em numeros inteiros
+// com Int cobrimos Dois Bilhões de numeros +/-
+// signed Int32
+// bit - 0 ou 1
+// byte - 8 bits - 00000000
+// 1, 11, 111 
+
+Console.WriteLine(age);
