270402980a5de0a8009938fb13198ceda4bf7c26
Arthur Cardoso <arthur.duartec@gmail.com>
2025-01-30 15:42:42 -0300

    Add files via upload

---
+++ Variáveis/bool.cs
@@ -0,0 +1,12 @@
+
+//true or false
+
+bool isComplete = false;
+
+isComplete = isComplete;
+isComplete = !isComplete;
+// usar o ! "flipa" a informação, de falso pra true e true pra falso
+// ! pode ser lido também como "is not" ou "não é"
+
+Console.WriteLine(isComplete);
+Console.WriteLine(!isComplete);
